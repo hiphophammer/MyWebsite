e9a614fdc8bd1712ee4ec32c7be76c18e68b5a38
Jeongwook Oh <hiphophammer@hotmail.com>
2023-01-19 23:52:21 -0800

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <hr className="h-px mt-4 bg-stone-400 border-0 dark:bg-stone-700" />
       <h2 className="mt-2">
         <a href="mailto: jeongwook0704@gmail.com" className="hover:underline">
-          Contact Me
+          Contact Me!
         </a>
       </h2>
       <h2 className="mt-2">© {new Date().getFullYear()}</h2>
